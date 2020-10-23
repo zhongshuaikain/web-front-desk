058cc782dcf710e09f58437e164a956ce86e9abf
zhongshuaikain <1667313912@qq.com>
2020-10-23 11:42:48 +0800

    数组

---
+++ src/main/webapp/WEB-INF/vue/9_vuex/9.2_Vuex值的存储和传递（计数器案例）/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!--    <h2>计数器的值为{{count}}</h2>-->
+    <!--    <button>+</button>-->
+    <!--    <button>-</button>-->
+    <!--   -->
+    <cpn>
+      <h2>计数器的值为{{$store.state.count}}</h2>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </cpn>
+  </div>
+</template>
+
+<script>
+  import cpn from './views/count'
+
+  export default {
+    name: 'App',
+    components: {
+      cpn
+    },
+    methods: {
+      increment() {
+        this.$store.commit('add')
+
+      },
+      decrement() {
+        this.$store.commit('jian')
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
